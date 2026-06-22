def6399e04e5ac9ea3dccba63c3cb86e9c67182c
satyajit <satyajit818181@gmail.com>
2026-06-22 13:10:41 +0530

    js start

---
+++ Month-1/Week-3/Day-1/Practice/script.js
@@ -0,0 +1,22 @@
+console.log("hello world");
+a=30
+console.log(a);
+
+A=40
+console.log(A);
+
+st1="ss";
+console.log(st1);
+
+$=70;
+console.log($)
+
+full_name="hii";
+console.log(full_name);
+
+//declare at function level
+function getV(){
+    var d=55;
+
+}
+console.log(d)
