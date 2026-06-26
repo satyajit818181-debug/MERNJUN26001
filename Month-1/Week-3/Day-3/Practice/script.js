47f83293af64244f759a1e508a46e1940a7af85a
satyajit <satyajit818181@gmail.com>
2026-06-26 22:10:40 +0530

    my first ui design

--- Month-1/Week-3/Day-3/Practice/script.js
+++ Month-1/Week-3/Day-3/Practice/script.js
@@ -7,7 +7,7 @@ console.log(3 > 5);
 console.log(6 < 10);
 console.log(3 >= 3);
 
-console.log(2 == 2);
+console.log(2 == 2); 
 console.log(5 == 6);
 console.log("hii" == "hii");
 console.log("hii" == 2);
